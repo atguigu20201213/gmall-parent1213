84aafc3fc50d58fc670acedd4169a77771ca76ca
atguigu20201213 <63486820+atguigu20201213@users.noreply.github.com>
2020-06-09 21:51:45 +0800

    2020-06-09  替换注释

--- service/service-product/src/main/java/com/atguigu/gmall1213/product/Controller/BaseManageController.java
+++ service/service-product/src/main/java/com/atguigu/gmall1213/product/Controller/BaseManageController.java
@@ -12,48 +12,61 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/**
- * 电商后台管理的控制
- */
-@Api("接口测试")
-@RestController  // @responseBody  + @controller
+        /**
+
+        * 电商后台管理的控制
+
+        */
+@Api("后台接口测试")
+@RestController // @ResponseBody + @Controller.
 @RequestMapping("admin/product")
 public class BaseManageController {
     @Autowired
     private ManageService manageService;
-
-    //书写一级分类
+    // http://api.gmall.com/admin/product/getCategory1
+    // 写谁 getCategory1 vue 项目页面需要获取的数据是Json 数据。
+    // 封装过一个结果集的类 Result
     @GetMapping("getCategory1")
-    public Result<List<BaseCategory1>> getCategory1() {
-        //最基本的返回方式
+    public Result<List<BaseCategory1>> getCategory1(){
+        // 最基本的返回方式！
         List<BaseCategory1> category1List = manageService.getCategory1();
+
         return Result.ok(category1List);
     }
-
-
+    // http://api.gmall.com/admin/product/getCategory2/{category1Id}
     @GetMapping("getCategory2/{category1Id}")
-    public Result getCategory2(@PathVariable Long category1Id) {
+    public Result getCategory2(@PathVariable Long category1Id){
+        // 根据一级分类Id 查询二级分类数据
         List<BaseCategory2> category2List = manageService.getCategory2(category1Id);
         return Result.ok(category2List);
     }
 
+    // http://api.gmall.com/admin/product/getCategory3/{category2Id}
     @GetMapping("getCategory3/{category2Id}")
-    public Result getCategory3(@PathVariable Long category2Id) {
+    public Result getCategory3(@PathVariable Long category2Id){
+        // 根据二级分类Id 查询三级分类数据
         List<BaseCategory3> category3List = manageService.getCategory3(category2Id);
         return Result.ok(category3List);
     }
 
+    // http://api.gmall.com/admin/product/attrInfoList/{category1Id}/{category2Id}/{category3Id}
     @GetMapping("attrInfoList/{category1Id}/{category2Id}/{category3Id}")
-    public Result getCategory3(@PathVariable Long category1Id, @PathVariable Long category2Id, @PathVariable Long category3Id) {
+    public Result getCategory3(@PathVariable Long category1Id,
+                               @PathVariable Long category2Id,
+                               @PathVariable Long category3Id){
+        // 根据分类Id 查询平台属性数据
         List<BaseAttrInfo> attrInfoList = manageService.getAttrInfoList(category1Id, category2Id, category3Id);
         return Result.ok(attrInfoList);
-
     }
-    //@RequestBody   将json 转换为java对象
+
+    // http://api.gmall.com/admin/product/saveAttrInfo
+    // 因为这个实体类中既有平台属性的数据，也有平台属性值的数据！
+    // vue 项目在页面传递过来的是json 字符串， 能否直接映射成java 对象？
+    // @RequestBody ： 将Json 数据转换为Java 对象。
     @PostMapping("saveAttrInfo")
-    public Result  saveAttrInfo(@RequestBody BaseAttrInfo baseAttrInfo){
+    public Result saveAttrInfo(@RequestBody  BaseAttrInfo baseAttrInfo){
+        // 调用保存方法
         manageService.saveAttrInfo(baseAttrInfo);
         return Result.ok();
     }
-
 }
--- service/service-product/src/main/java/com/atguigu/gmall1213/product/service/Impl/ManageServiceImpl.java
+++ service/service-product/src/main/java/com/atguigu/gmall1213/product/service/Impl/ManageServiceImpl.java
@@ -7,75 +7,96 @@ import com.atguigu.gmall1213.product.service.ManageService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.data.annotation.Transient;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-
 public class ManageServiceImpl implements ManageService {
+
+    // 通常会调用mapper 层。
     @Autowired
     private BaseCategory1Mapper baseCategory1Mapper;
 
     @Autowired
     private BaseCategory2Mapper baseCategory2Mapper;
+
     @Autowired
     private BaseCategory3Mapper baseCategory3Mapper;
-    @Autowired
-    private BaseAttrValueMapper baseAttrValueMapper;
+
     @Autowired
     private BaseAttrInfoMapper baseAttrInfoMapper;
 
+    @Autowired
+    private BaseAttrValueMapper baseAttrValueMapper;
+
     @Override
     public List<BaseCategory1> getCategory1() {
+        // select * from base_category1; 表与实体类与mapper 名称对应！
         return baseCategory1Mapper.selectList(null);
     }
 
     @Override
     public List<BaseCategory2> getCategory2(Long category1Id) {
+        // select * from base_category2 where category1_id=category1Id;
         QueryWrapper<BaseCategory2> baseCategory2QueryWrapper = new QueryWrapper<>();
-        baseCategory2QueryWrapper.eq("category1_Id", category1Id);
-        return baseCategory2Mapper.selectList(baseCategory2QueryWrapper);
+        // 第一个参数，是实体类的属性名，还是字段名？
+        baseCategory2QueryWrapper.eq("category1_id",category1Id);
+        return   baseCategory2Mapper.selectList(baseCategory2QueryWrapper);
     }
 
     @Override
     public List<BaseCategory3> getCategory3(Long category2Id) {
+        // select * from base_category3 where category2_id=category2Id;
         QueryWrapper<BaseCategory3> baseCategory3QueryWrapper = new QueryWrapper<>();
-        baseCategory3QueryWrapper.eq("category2_Id", category2Id);
+        baseCategory3QueryWrapper.eq("category2_id",category2Id);
         return baseCategory3Mapper.selectList(baseCategory3QueryWrapper);
-
     }
 
     @Override
     public List<BaseAttrInfo> getAttrInfoList(Long category1Id, Long category2Id, Long category3Id) {
+        /*
+         如果说只根据分类Id 查询平台属性!
+         select * from base_attr_info where category_id=category1Id and category_level = 1 or
+         select * from base_attr_info where category_id=category2Id and category_level = 2 or
+         select * from base_attr_info where category_id=category3Id and category_level = 3 or
+         category_level 表示层级关系
+         category1Id category_level = 1
+         category2Id category_level = 2
+         category3Id category_level = 3
+        -----------------------------------------------------------------
+         扩展功能： 我们需要根据分类Id ，需要得到属性名，最好还能得到属性值名称。
+            通过分类Id 能够得到属性名，
+         base_attr_value 属性值数据在这张表中！
 
-        return baseAttrInfoMapper.selectBaseAttrInfoList(category1Id, category2Id, category3Id);
+         */
+        return baseAttrInfoMapper.selectBaseAttrInfoList(category1Id,category2Id,category3Id);
     }
 
     @Override
-    @Transient
+    @Transactional
     public void saveAttrInfo(BaseAttrInfo baseAttrInfo) {
-        if (null != baseAttrInfo) {
-            /**
-             * baseAttrInfo  平台属性表
-             *baseAttrValue  平台属性值
+        if (null!=baseAttrInfo){
+            /*
+            一个是平台属性表；baseAttrInfo
+            一个是平台属性值表： baseAttrValue
              */
             baseAttrInfoMapper.insert(baseAttrInfo);
-
+            // 平台属性值插入的时候，可能存在多个值的去情况，具体是多少个值，需要看传递过来的数据。
+            // 页面在传递平台属性值数据的时候，数据会自动封装到 BaseAttrInfo 中 这个属性中 attrValueList
+            //  前台页面给封装好的！ int i = 1/0;
             List<BaseAttrValue> attrValueList = baseAttrInfo.getAttrValueList();
-            if (null != attrValueList && attrValueList.size() > 0) {
-
+            if (null!=attrValueList && attrValueList.size()>0){
                 for (BaseAttrValue baseAttrValue : attrValueList) {
-                    //页面在提交数据的时候 没有给attrId赋值 所有在此时手动赋值
-                    //attrID = baseAttrInfo.getId
+                    // 页面在提交数据的时候，并没有给attrId 赋值，所以在此处需要手动赋值
+                    // attrId = baseAttrInfo.getId();
                     baseAttrValue.setAttrId(baseAttrInfo.getId());
-                    //循环将数据添加到数据表中
+                    // 循环将数据添加到数据表中
                     baseAttrValueMapper.insert(baseAttrValue);
-
                 }
             }
         }
-
     }
 }
