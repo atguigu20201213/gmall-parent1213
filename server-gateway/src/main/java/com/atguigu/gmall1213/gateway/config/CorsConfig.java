8c0284e64d0644f2521b54a8ee1f2923405cd900
atguigu20201213 <63486820+atguigu20201213@users.noreply.github.com>
2020-07-04 16:38:58 +0800

    2020-7-4-2

--- server-gateway/src/main/java/com/atguigu/gmall1213/gateway/config/CorsConfig.java
+++ server-gateway/src/main/java/com/atguigu/gmall1213/gateway/config/CorsConfig.java
@@ -8,20 +8,21 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
-    @Bean
+      @Bean
     public CorsWebFilter corsWebFilter(){
 
-        // cors跨域配置对象
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*"); //设置允许访问的网络
-        configuration.setAllowCredentials(true); // 设置是否从服务器获取cookie
-        configuration.addAllowedMethod("*"); // 设置请求方法 * 表示任意
-        configuration.addAllowedHeader("*"); // 所有请求头信息 * 表示任意
+        // 创建corsconfiguration();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 设置跨域的属性
+        corsConfiguration.addAllowedOrigin("*"); // 设置允许访问的网络
+        corsConfiguration.setAllowCredentials(true); // 表示是否从服务器中获取到cookie ，如果允许true，否则false。
+        corsConfiguration.addAllowedMethod("*"); // 表示允许所有的请求方法
+        corsConfiguration.addAllowedHeader("*"); //  表示设置请求头信息{设置任意参数}
 
-        // 配置源对象
-        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
-        configurationSource.registerCorsConfiguration("/**", configuration);
-        // cors过滤器对象
-        return new CorsWebFilter(configurationSource);
+        // 创建source
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
+        // 返回CorsWebFilter
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 }
